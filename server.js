cda69fa5f19a72bc8d044848f68aef0449415ace
BDicksmusic <Bdicksmusic@gmail.com>
2025-07-19 14:49:04 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Serve static files
 app.use(express.static('public'));
 
+// Test route
 app.get('/', (req, res) => {
-    res.send('<h1>Hello from My Music Catalog!</h1>');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Use Railway's port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
