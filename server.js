a270fb3710f3a40a87fc21454013ee7de95fddd4
BDicksmusic <Bdicksmusic@gmail.com>
2025-07-19 14:56:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('<h1>🎵 Music Catalog - Test Version</h1><p>Server is working!</p>');
 });
 
+app.get('/test', (req, res) => {
+    res.send('<h1>TEST ROUTE WORKING!</h1>');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`✅ SIMPLE SERVER RUNNING ON PORT: ${PORT}`);
